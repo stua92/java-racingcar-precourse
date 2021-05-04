caa9d328d4e52c271eeb8ecf70d76c838b329c30
stua92 <stau92@gmail.com>
2021-05-04 22:03:40 +0900

    feat: 입력클래스, 유틸 클래스 구현

---
+++ src/main/java/AutoRacing/InputScanner.java
@@ -0,0 +1,29 @@
+package AutoRacing;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class InputScanner {
+	final static int ZERO = 0;
+	static Scanner scanner = new Scanner(System.in);
+
+	public static RunningCars createCars() {
+		String input = scanner.nextLine();
+		RunningCars cars;
+		cars = new RunningCars(splitToCarArrayList(input));
+		return cars;
+	}
+
+	public static ArrayList<Car> splitToCarArrayList(String input) {
+		String[] carNameAry = input.split(",");
+		ArrayList<Car> cars = new ArrayList<Car>();
+		for (String carName : carNameAry) {
+			cars.add(new Car(carName, ZERO, ZERO));
+		}
+		return cars;
+	}
+
+	public static int setRoundCount() {
+		return scanner.nextInt();
+	}
+}
---
+++ src/main/java/utils/ValueState.java
@@ -0,0 +1,5 @@
+package utils;
+
+public class ValueState {
+	public final static int ZERO = 0;
+}
