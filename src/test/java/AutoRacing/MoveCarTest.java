7d868965b40e319f6d069a0a3c759b085c8500c6
stua92 <stau92@gmail.com>
2021-05-04 22:07:24 +0900

    test: 자동차 STOP or RUN 테스트

---
+++ src/test/java/AutoRacing/MoveCarTest.java
@@ -0,0 +1,17 @@
+package AutoRacing;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import utils.CarAction;
+
+public class MoveCarTest {
+	@Test
+	@DisplayName("자동차 움직임 테스트")
+	void validationCheckTest() {
+		assertThat(MoveCar.checkMotion(3) == CarAction.STOP).isTrue();
+		assertThat(MoveCar.checkMotion(4) == CarAction.RUN).isTrue();
+	}
+}
